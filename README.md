2ab7eaf6f18574417948263f0720f6684711e5c8
Sprinkle7 <allauddin2015@gmail.com>
2022-09-06 12:07:42 +0500

    Added New Functions, Update Readme File

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# utils
-∏
+# Chuphsa  📝  
+Everyday use functions.
+
+## Installation 🚀  
+
+Via npm:
+
+    $ npm install chuspha
+
+## Usage
+
+```js
+    cont chuspha = require('chupsha');
+
+    var toBeCapitalized = 'hello world';
+    // Capitalize String
+    console.log(chupsha.cap(toBeCapitalized));
+
+```
+## OR
+
+```js
+    const { cap } = require('chupsha');
+
+    var toBeCapitalized = 'hello world';
+    // Capitalize String
+    console.log(cap(toBeCapitalized));
+
+```
+
+### Library Includes
+
+##### unique(array)
+Remove duplicate values from an array
+
+##### removeindex(array,index)
+Remove an object from an array
+
+##### sum(array)
+Sum all the values in the array
+
+##### soras(array, key, boolean)
+Sort an array of objects by a key with string value. Boolean for Asc & Desc
+
+##### soran(array, key, boolean)
+Sort an array of objects by a key with number value. Boolean for Asc & Desc
+
+##### exists(array, key, value)
+Checking if key contains a value in array
+
+##### min(array)
+Return min value from array
+
+##### max(max)
+Return max value from array
+
+##### obj(array)
+Return first object of array or convert single array to object
+
+##### len(array)
+Get array length
+
+##### hash(length)
+Generate A Random Hash String
+
+##### token6()
+Generate A Random 6 digit token
+
+##### token4()
+Generate A Random 4 digit token
+
+##### replace(str, replace, by)
+Replace a string in string
+
+##### upper(str)
+Convert the string to uppercase
+
+##### lower(str)
+Convert the string to lowercase
+
+##### cap(str)
+Capitalize the first letter of the string
+
+##### capword(str)
+Capitalize the first letter of each word in string
+
+##### trim(str)
+Trim a string
+
+##### pads(str,limit)
+Adding 0 to the start of the string
+
+##### split(str, opt = '')
+Split the string into an array
+
+##### join(str,opt = '')
+Join array into a string
+  
+##### removechar(str, opt)
+Remove a character and capitalizing the first letter of the word
+   
+##### empty(val)
+Check if string, array or object is empty or null
+  
+##### url(val)
+Check if valid URL
+
+##### email(val)
+Check if valid Email
+
+##### iban(val)
+Check if Iban is valid
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const { isValidIBANNumber } = require('./iban');
 module.exports = {
+    /* Removing duplicate values from an array. */
     unique: (array) => {
         function unique(value, index, self) {
             return self.indexOf(value) === index;
         }
         return array.filter(unique);
     },
-    removeItem: (array, index) => { 
-        array.splice(index,1);
+    /* Removing an object from an array. */
+    removeindex: (array, index) => {
+        array.splice(index, 1);
         return array;
     },
+    /* Adding all the values in the array. */
     sum: (array) => array.reduce((partialSum, a) => partialSum + a, 0),
-    sortArrayByString: (array, pointer, by) => {
+    /* Sorting an array of objects by a pointer. */
+    soras: (array, pointer, by) => {
         if (by) {
             return array.sort((a, b) => (a[pointer] > b[pointer]) ? 1 : ((b[pointer] > a[pointer]) ? -1 : 0));
         } else {
@@ -18,65 +23,93 @@ module.exports = {
             return array.sort((a, b) => (a[pointer] < b[pointer]) ? 1 : ((b[pointer] < a[pointer]) ? -1 : 0));
         }
     },
-    sortArrayByNumber: (array, pointer, by) => {
+    /* Sorting an array of objects by a pointer. */
+    soran: (array, pointer, by) => {
         if (by) {
             return array.sort((a, b) => parseFloat(a[pointer]) - parseFloat(b[pointer]));
         } else {
             return array.sort((a, b) => parseFloat(b[pointer]) - parseFloat(a[pointer]));
         }
     },
-    exists: (array, [pointer, value]) => {
+    /* Checking if the value exists in the array. */
+    exists: (array, pointer, value) => {
         return array.some(el => el[pointer] === value);
     },
-    minValue: (array) => {
+    /* Sorting the array and returning the first value. */
+    min: (array) => {
         var data = array.sort(function(a, b) { return a - b });
         return data[0];
     },
-    maxValue: (array) => {
+    /* Sorting the array and returning the first value. */
+    max: (array) => {
         var data = array.sort(function(a, b) { return b - a });
         return data[0];
     },
+    /* Returning the first object in the array. */
     obj: (array) => array[0],
-    day: () => {
+    /* Returning the current date in ISO format. */
+    today: () => {
         const d = new Date();
         return d.toISOString();
     },
-    justDate: () => {
+    /* Returning the current date in ISO format. */
+    justdate: () => {
         const d = new Date().toISOString().split('T');
         return d[0];
     },
-    DateString: (date = null) => {
+    /* Returning the date in a string format. */
+    datestring: (date = null) => {
         if (date) {
             return new Date(date).toDateString();
         } else {
             return new Date().toDateString();
         }
     },
-    length: (array) => array.length,
-    replace: (str, [a, b]) => str.replace(a, b),
+    /* Returning the length of the array. */
+    len: (array) => array.length,
+    /* Replacing the value of a with b. */
+    replace: (str, a, b) => str.replace(a, b),
+    /* Converting the string to uppercase. */
     upper: (str) => str.toUpperCase(),
+    /* Converting the string to lowercase. */
     lower: (str) => str.toLowerCase(),
+    /* Capitalizing the first letter of the string. */
+    cap: (string) => string.charAt(0).toUpperCase() + string.slice(1),
+    /* Capitalizing the first letter of each word in the string. */
+    capword: (str) => {
+        var splitStr = str.toLowerCase().split(' ');
+        for (var i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        return splitStr.join(' ');
+    },
+    /* Trimming the string. */
     trim: (str) => str.trim(),
-    pads: (str) => str.padStart(6, 0),
-    split: (str, opt) => str.split(opt),
-    join: (str, opt) => str.join(opt),
+    /* Adding 0 to the start of the string. */
+    pads: (str, limit) => str.padStart(limit, 0),
+    /* Splitting the string into an array. */
+    split: (str, opt = '') => str.split(opt),
+    /* Joining the array into a string. */
+    join: (array, opt = '') => array.join(opt),
+    /* Generating a random string of characters. */
     hash: (length) => {
         var result = '';
-        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz0123456789';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXZabcdefghijklmnopqrstuvwxyz0123456789@#$%^&*!';
         var charactersLength = characters.length;
         for (var i = 0; i < length; i++) {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
         return result;
     },
-    removeChar: (str, opt) => {
+    /* Removing the character and capitalizing the first letter of the word. */
+    removechar: (str, opt) => {
         var i, frags = str.split(opt);
         for (i = 0; i < frags.length; i++) {
             frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
         }
         return frags.join(' ');
     },
-    isEmpty: (obj) => {
+    empty: (obj) => {
         if (obj == null) return true;
         if (obj.length > 0) return false;
         if (obj.length === 0) return true;
@@ -86,7 +119,8 @@ module.exports = {
         }
         return true;
     },
-    validLink: (str) => {
+    /* Checking if the string is a valid link. */
+    url: (str) => {
         var pattern = new RegExp('^(http?:\\/\\/)?' +
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
             '((\\d{1,3}\\.){3}\\d{1,3}))' +
@@ -94,5 +128,14 @@ module.exports = {
             '(\\?[;&a-z\\d%_.~+=-]*)?' +
             '(\\#[-a-z\\d_]*)?$', 'i');
         return !!pattern.test(str);
-    }
+    },
+    /* Checking if the email is valid. */
+    email: (email) => {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        return regex.test(email);
+    },
+    iban: (id) => isValidIBANNumber(id),
+    token6: () => Math.floor(100001 + Math.random() * 899998),
+    token4: () => Math.floor(1001 + Math.random() * 8998),
+
 }
